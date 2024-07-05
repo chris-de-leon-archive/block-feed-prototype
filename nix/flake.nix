b06127617337800e25ba10f42dbcc773baae1c05
chris-de-leon <chrisdeleon333+dev@gmail.com>
2024-07-05 07:04:33 +0000

    update nix packages

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,34 +1,26 @@
-# https://search.nixos.org/packages?channel=unstable&show=go&from=0&size=50&sort=relevance&type=packages&query=go
-#
-# Run with:
-#
-#   NIXPKGS_ALLOW_UNFREE=1 nix develop --impure ./nix
-#
+# https://github.com/NixOS/nixpkgs/commit/9f4128e00b0ae8ec65918efeba59db998750ead6 
 {
   inputs = {
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/3f59355d8466fc2b1cfe45cd21e4476a686bfeaf.tar.gz";
-    flowcli.url = "https://github.com/chris-de-leon/flow-cli.nix/archive/refs/tags/v1.17.1.tar.gz";
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/9f4128e00b0ae8ec65918efeba59db998750ead6.tar.gz";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flowcli, utils }:
+  outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem(system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ flowcli.overlay ];
         };
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.terraform # v1.7.5
-            pkgs.nodejs_21 # v21.7.2
-            pkgs.redis # v7.2.4
-            pkgs.flow # v1.17.1
-            pkgs.go #v1.22.2
+            # pkgs.terraform # v1.8.5
+            pkgs.nodejs_22 # v22.3.0
+            pkgs.redis # v7.2.5
+            pkgs.go #v1.22.3
             pkgs.sqlc # v1.26.0
-            pkgs.atlas # v0.21.1
-            pkgs.stripe-cli # v1.19.4
+            pkgs.atlas # v0.24.0
+            pkgs.stripe-cli # v1.21.0
           ];
 
           shellHook = ''
--- setup.sh
+++ setup.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-NIXPKGS_ALLOW_UNFREE=1 nix develop --impure ./nix
+NIXPKGS_ALLOW_UNFREE=1 \
+  nix \
+  --extra-experimental-features 'flakes' \
+  --extra-experimental-features 'nix-command' \
+  develop \
+  --show-trace \
+  --impure \
+  ./nix
