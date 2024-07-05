440f3cbc43618b96d279e8b26ee8e6cbcbb06b61
chris-de-leon <chrisdeleon333+dev@gmail.com>
2024-07-05 02:22:34 -0700

    clean up compose files and update pnpm

--- nix/flake.nix
+++ nix/flake.nix
@@ -16,16 +16,13 @@
           buildInputs = [
             # pkgs.terraform # v1.8.5
             pkgs.nodejs_22 # v22.3.0
+            pkgs.nodejs_22.pkgs.pnpm # v9.3.0
             pkgs.redis # v7.2.5
             pkgs.go #v1.22.3
             pkgs.sqlc # v1.26.0
             pkgs.atlas # v0.24.0
             pkgs.stripe-cli # v1.21.0
           ];
-
-          shellHook = ''
-            corepack enable pnpm
-          '';
         };
       }
     );
