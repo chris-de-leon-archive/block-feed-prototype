cd4aae221b65c4a22e7986d6c98ebad4c80ea1b3
chris-de-leon <135092418+chris-de-leon@users.noreply.github.com>
2025-06-11 11:03:12 +0000

    feat: migrate to infisical

--- flake.nix
+++ flake.nix
@@ -7,46 +7,31 @@
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        bws = import ./nix/bws { inherit pkgs; };
-        tf = import ./nix/tf { inherit pkgs; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
       in
       {
-        formatter = pkgs.nixpkgs-fmt;
+        formatter = pkgs.alejandra;
 
         devShells = {
           default = pkgs.mkShell {
             GOROOT = "${pkgs.go}/share/go";
 
-            # NOTE: it's much faster to install the Bitwarden secrets CLI
-            # and the Terraform CLI via Docker instead of Nix. Installing
-            # via Docker also removes the need for NIXPKGS_ALLOW_UNFREE=1
-            # and `--impure` for Nix shells
             packages = [
               pkgs.nodejs_22.pkgs.pnpm
               pkgs.nodejs_22
               pkgs.stripe-cli
+              pkgs.infisical
+              pkgs.terraform
               pkgs.redis
               pkgs.atlas
               pkgs.sqlc
               pkgs.go
-              bws
-              tf
             ];
-
-            # NOTE: `bws secret list` will NOT return an empty list if the project contains no secrets. Instead it will fail with a 404 error. To fix
-            # this, there needs to be at least one dummy secret added to the project. There's a thread about this linked below but it has no activity
-            # https://community.bitwarden.com/t/bws-secret-list-project-id-shouldnt-return-a-404-if-there-are-no-secrets-in-the-project/64668
-            shellHook = ''
-              export BWS_PROJECT_ID=""
-              if [[ -f ./.bws ]]; then
-                BWS_PROJECT_ID="$(cat .bws)"
-              else
-                BWS_PROJECT_ID="$(bws project create 'block-feed-prototype' | jq -erc '.id')"
-                bws secret create 'PROJECT_ID' "$BWS_PROJECT_ID" "$BWS_PROJECT_ID"
-                echo -n "$BWS_PROJECT_ID" > .bws
-              fi
-            '';
           };
         };
       }
